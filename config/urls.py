9d9d99fff480845a581d82912c11bedcba429d31
student <jasurabdibayev0@gmail.com>
2022-12-02 16:13:49 +0500

    del redoc

--- config/urls.py
+++ config/urls.py
@@ -39,7 +39,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger<format>.json|.yaml/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('account/', include('accaunt.urls')),
     path('todo/', include('todo_api.urls'))
 
